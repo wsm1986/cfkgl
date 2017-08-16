3f02686cdcb6aecc75e5bc8928f80fb545deacb6
wellington <wsm1986@gmail.com>
2017-08-16 20:04:01 -0300

    adicionando permissao

--- src/main/java/com/kgl/controller/HomeController.java
+++ src/main/java/com/kgl/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,10 +21,11 @@ import com.kgl.repository.UserRepository;
 @Controller
 public class HomeController {
 
-
-	
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	ConfigurarUser conf;
 	
 	@RequestMapping({ "/", "/index" })
 	private ModelAndView form(Contrato contrato) {
@@ -35,17 +37,18 @@ public class HomeController {
 	@Transactional
 	@ResponseBody
 	@RequestMapping("/url-magica-maluca-/{email}")
-	public String urlMagicaMaluca(@PathVariable("email") String email) {
+	public ModelAndView urlMagicaMaluca(@PathVariable("email") String email) throws Exception {
 		String password;
 		List<Role> list = new ArrayList();
-		password = GenerateHashPasswordUtil.generateHash("1234");
+		password = GenerateHashPasswordUtil.generateHash("1425");
 		Role role = new Role();
-		role.setNome("ROLE_USER");
+		role.setNome("ROLE_ADMIN");
 		list.add(role);
 		User user = new User(email, password, list);
 		userRepository.save(user);
 
-		return "/login";
+		return conf.updatePassword(user);
 	}
+
 
 }
